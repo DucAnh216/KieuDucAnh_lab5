bd2b8f433aebc811ccd00c76a21e7175421738e4
DucAnh216 <kieuducanhcts29@gmail.com>
2025-04-17 18:19:42 +0700

    Add project files.

---
+++ ASC.Web/Configuration/ApplicationSettings.cs
@@ -0,0 +1,26 @@
+﻿namespace ASC.Web.Configuration
+{
+    public class ApplicationSettings
+    {
+        public required string ApplicationTitle { get; set; }
+
+        // Admin Information
+        public required string AdminEmail { get; set; }
+        public required string AdminName { get; set; }
+        public required string AdminPassword { get; set; }
+
+        // User Roles
+        public required string Roles { get; set; }
+
+        // Engineer Information
+        public required string EngineerEmail { get; set; }
+        public required string EngineerName { get; set; }
+        public required string EngineerPassword { get; set; }
+
+        // SMTP Configuration
+        public required string SMTPServer { get; set; }
+        public required int SMTPPort { get; set; }
+        public required string SMTPAccount { get; set; }
+        public required string SMTPPassword { get; set; }
+    }
+}
